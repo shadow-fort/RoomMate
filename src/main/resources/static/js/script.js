f1dc9b8bb3d6ddf2bd209671c95bbedac282c3c8
shadow-fort <ytague545@gmail.com>
2026-04-23 13:00:10 +0200

    Uptdate User Interfaces

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,63 +1,120 @@
-function togglePopupForBooking() {
+function togglePopupForBooking(workplace) {
     document.getElementById("popup-1").classList.toggle("active");
-
-    // Get date and time elements
-    const dateInput = document.getElementById("dateID");
-    const startTimeInput = document.getElementById("startTimeID");
-    const endTimeInput = document.getElementById("endTimeID");
-    const submitButton = document.getElementById("submitBtn");
-
-    submitButton.addEventListener("click", function (event) {
-        event.preventDefault(); // Avoid form submission
-
-        // Get current Date and Time
-        const currentDate = new Date();
-        const currentTime = new Date();
-
-        // Get User selected Date
-        const selectedDate = new Date(dateInput.value.trim()); // Trim leading/trailing spaces
-
-        // Get User selected Start Time
-        const selectedStartTime = new Date(`2000-01-01T${startTimeInput.value}:00`);
-
-        // Get User selected End Time
-        const selectedEndTime = new Date(`2000-01-01T${endTimeInput.value}:00`);
-
-        // Check if selected Date, Start Time, or End Time is empty or in the past
-        if (selectedDate < currentDate) {
-            alert("No date selected or the date is in the past.");
-        } else if (selectedDate.getTime() < currentDate.getTime()) {
-            alert("The selected time is in the past for the current date.");
-        } else if (selectedStartTime >= selectedEndTime) {
-            alert("The end time must be later than the start time.");
-        } else {
-            // Submit the Form
-            document.forms[0].submit();
-        }
-    });
+    var selectedWorkplaceID = document.getElementById("selectedWorkplaceID");
+    selectedWorkplaceID.value = workplace;
+}
+function togglePopupForBookingForSearchCriteria(workplace, roomId, selectedDescription) {
+    document.getElementById("popup-1").classList.toggle("active");
+    var selectedWorkplaceID = document.getElementById("selectedWorkplaceID");
+    selectedWorkplaceID.value = workplace;
+    var selectedRoomID = document.getElementById("selectedRoomID");
+    selectedRoomID.value = roomId;
+    var selectedDescriptionID = document.getElementById("selectedDescriptionID");
+    selectedDescriptionID.value = selectedDescription;
 }
 
-function togglePopupForStatusCheking(){
+function togglePopupForStatusCheking(workplaceNumber) {
     document.getElementById("popup-2").classList.toggle("active");
-
+    //var wn = document.getElementsByName("workplaceNumberForCheckingBookings").values();
+    //document.createAttribute("workplaceNumberForCheckingBookings").value = workplaceNumber;
+    var wn = document.createAttribute("workplaceNumberForCheckingBookings").name;
+    //wn.value = workplaceNumber;
 }
 
 
-function togglePopupForUpdatingBook(){
+function togglePopupForDeletingBook(bookingID) {
+    document.getElementById("popup-11").classList.toggle("active");
+    var bookingToDeleteID = document.getElementById("bookingToDeleteID");
+    bookingToDeleteID.value = bookingID;
+}
+
+function togglePopupForDeletingRoom(roomID, roomName) {
+    document.getElementById("popup-12").classList.toggle("active");
+    var roomToDeleteID = document.getElementById("roomToDeleteID");
+    roomToDeleteID.value = roomID;
+    var roomNameToDelete = document.getElementById("roomNameToDelete");
+    roomNameToDelete.value = roomName;
+}
+
+function togglePopupForUpdatingBook(bookingID, bookingDate, bookingStart, bookingEnd, bookingRoomName, bookingWorkplaceNumber) {
     document.getElementById("popup-3").classList.toggle("active");
-
+    var bookingToUpdateID = document.getElementById("bookingToUpdateID");
+    bookingToUpdateID.value = bookingID;
+    var bookingForDate = document.getElementById("bookingForDate");
+    bookingForDate.value = bookingDate;
+    var bookingForStart = document.getElementById("bookingForStart");
+    bookingForStart.value = bookingStart;
+    var bookingForEnD = document.getElementById("bookingForEnD");
+    bookingForEnD.value = bookingEnd;
+    var bookingForRoom = document.getElementById("bookingForRoom");
+    bookingForRoom.value = bookingRoomName;
+    var bookingForWorkplace = document.getElementById("bookingForWorkplace");
+    bookingForWorkplace.value = bookingWorkplaceNumber;
 }
 
-function togglePopupForAddingNewRoom(){
+function togglePopupForAddingNewRoom() {
     document.getElementById("popup-4").classList.toggle("active");
 
 }
-function togglePopupForAddingNewWorkplace(){
+
+function togglePopupForBookingBlocked() {
+    document.getElementById("popup-14").classList.toggle("active");
+
+}
+
+function togglePopupForAddingNewWorkplace() {
     document.getElementById("popup-5").classList.toggle("active");
 
 }
 
-function togglePopupForEditingWorkplace(){
+function togglePopupForAvailableWorkplace(roomID, workplaceNumber, workplaceStatus, equipmentDescription) {
     document.getElementById("popup-6").classList.toggle("active");
+    var selectedRoomId = document.getElementById("selectedRoomId");
+    selectedRoomId.value = roomID;
+    var selectedWorkplaceNumber = document.getElementById("selectedWorkplaceNumber");
+    selectedWorkplaceNumber.value = workplaceNumber;
+    var currentStatus = document.getElementById("currentStatus");
+    currentStatus.value = workplaceStatus;
+    var selectedEquipment = document.getElementById("selectedEquipment");
+    selectedEquipment.value = equipmentDescription;
+}
+
+function togglePopupForSearching(roomID) {
+    document.getElementById("popup-7").classList.toggle("active");
+    var selectedtRoom = document.getElementById("roomIDForSearching");
+    selectedtRoom.value = roomID;
 
 }
+
+function togglePopupForAdminSearching() {
+    document.getElementById("popup-8").classList.toggle("active");
+}
+
+function togglePopupForAdminEditRoom(roomId, roomName) {
+    document.getElementById("popup-9").classList.toggle("active");
+    var selectedRoomId = document.getElementById("roomId");
+    selectedRoomId.value = roomId;
+    var selectedRoomName = document.getElementById("RoomNameID");
+    selectedRoomName.value = roomName;
+
+
+}
+
+function togglePopupForDeletingWorkplace(workplaceNumber) {
+    document.getElementById("popup-10").classList.toggle("active");
+    var workplaceNumberID = document.getElementById("workplaceNumberID");
+    workplaceNumberID.value = workplaceNumber;
+}
+
+// hides the flash attribute after 10 seconds for success message
+setTimeout(function () {
+    document.getElementById("success-message").style.display = "none";
+}, 3000);
+
+// hides the flash attribute after 10 seconds for success message
+setTimeout(function () {
+    document.getElementById("error-message").style.display = "none";
+}, 3000);
+
+
+
