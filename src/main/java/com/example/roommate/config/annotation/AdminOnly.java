444a950614aee66ac425a33e7ca81e760f1f3595
shadow-fort <ytague545@gmail.com>
2026-04-23 13:02:02 +0200

    Implementierung eigene Annotationen

---
+++ src/main/java/com/example/roommate/config/annotation/AdminOnly.java
@@ -0,0 +1,16 @@
+package com.example.roommate.config.annotation;
+
+import org.springframework.security.access.annotation.Secured;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+@Secured("ROLE_ADMIN")
+public @interface AdminOnly {
+}
